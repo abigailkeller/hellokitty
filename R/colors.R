631441c333a00c2bca20a4cda46275421ee70bd5
abigailkeller <abigail.g.keller1@gmail.com>
2026-06-02 05:18:33 +0000

    added workflow

--- R/colors.R
+++ R/colors.R
@@ -10,11 +10,7 @@ hkitty_palettes <- list(
 #'
 #' These are a handful of color palettes from Wes Anderson movies.
 #'
-#' @param n Number of colors desired. Unfortunately most palettes now only
-#'   have 4 or 5 colors. But hopefully we'll add more palettes soon. All color
-#'   schemes are derived from the most excellent Tumblr blog:
-#'   \href{http://wesandersonpalettes.tumblr.com/}{Wes Anderson Palettes}.
-#'   If omitted, uses all colours.
+#' @param n Number of colors desired. If omitted, uses all colours.
 #' @param name Name of desired palette. Choices are:
 #'   \code{hellokitty1}, \code{hellokitty2}
 #' @param type Either "continuous" or "discrete". Use continuous if you want
@@ -32,14 +28,11 @@ hkitty_palettes <- list(
 #' # can use a continuous palette to interpolate between existing
 #' # colours
 #' pal <- hkitty_palette(21, name = "hellokitty2", type = "continuous")
-#' image(volcano, col = pal)
 hkitty_palette <- function(name, n, type = c("discrete", "continuous")) {
+  
   type <- match.arg(type)
   
   pal <- hkitty_palettes[[name]]
-  if (type == "continuous" && name == "Zissou1") {
-    pal <- wes_palettes[["Zissou1Continuous"]]
-  }
   
   if (is.null(pal))
     stop("Palette not found.")
@@ -73,8 +66,3 @@ print.palette <- function(x, ...) {
   rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
   text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
 }
-
-#' heatmap
-#'
-#' A heatmap example
-"heatmap"
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Hello Kitty Color Palettes
 
+<!-- badges: start -->
+
+[![R-CMD-check](https://github.com/abigailkeller/hellokitty/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/abigailkeller/hellokitty/actions/workflows/R-CMD-check.yaml)
+
+<!-- badges: end -->
+
 ## Installation
 
 ``` r
@@ -22,8 +28,9 @@
 
 ## Palettes
 
-<img src="figure/hellokitty1.webp" style="height:2in" /> [Image
-Source](https://hellokitty.fandom.com/wiki/Hello_Kitty)
+<img src="figure/hellokitty1.webp" style="height:2in" />
+
+[Image Source](https://hellokitty.fandom.com/wiki/Hello_Kitty)
 
 ``` r
 #hkitty_palette("hellokitty1")
@@ -37,3 +44,9 @@ Source](https://www.parents.com/why-is-hello-kitty-so-popular-11851589)
 ``` r
 #hkitty_palette("hellokitty2")
 ```
+
+### Notes
+
+Code adapted from the
+[wesanderson](https://github.com/karthik/wesanderson/tree/master) R
+package.
